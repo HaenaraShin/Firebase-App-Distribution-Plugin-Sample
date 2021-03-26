c38f1ce8066ee9193b47d710f3142407d57f96f7
HaenaraShin <hamster12345@gmail.com>
2021-03-27 01:46:07 +0900

    feat(ui): 로티애니메이션 추가

--- app/src/main/java/dev/haenara/sample/firebase/app/MainActivity.kt
+++ app/src/main/java/dev/haenara/sample/firebase/app/MainActivity.kt
@@ -2,10 +2,22 @@ package dev.haenara.sample.firebase.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.airbnb.lottie.Lottie
+import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val lottieView: LottieAnimationView by lazy {
+        findViewById<LottieAnimationView>(R.id.lottie_view)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        with(lottieView) {
+            setAnimation("rocket.json")
+            playAnimation()
+        }
     }
 }
